0623a974b74ef19fdbae6bd22bf29390a6f7386b
HAoRAn-W <haoran2326@gmail.com>
2021-12-24 14:35:28 -0500

    turn off debug

--- RyanBlog/settings.py
+++ RyanBlog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_mu7u$r5&1%dv!0p7u1(lk91)3dv-ru(9_!nusijqocbn(^*4t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '20.124.206.155', '.whr.one']
 
