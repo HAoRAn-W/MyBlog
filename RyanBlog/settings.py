2dd4b2aca36000eebd145d51166526bf3511dc41
HAoRAn-W <haoran2326@gmail.com>
2021-12-26 23:09:08 -0500

    modify html files

--- RyanBlog/settings.py
+++ RyanBlog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_mu7u$r5&1%dv!0p7u1(lk91)3dv-ru(9_!nusijqocbn(^*4t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '20.124.206.155', '.whr.one']
 
@@ -115,7 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # django-pure-pagination 分页设置
 PAGINATION_SETTINGS = {
