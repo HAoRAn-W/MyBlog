756f8b70a45de825fcd950e4a7ec2cd448401321
HAoRAn-W <haoran2326@gmail.com>
2021-12-27 00:13:30 -0500

    add automatic deployment

--- RyanBlog/settings/production.py
+++ RyanBlog/settings/production.py
@@ -2,4 +2,4 @@ from .common import *
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.whr.one']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','20.124.206.155', '.whr.one']
