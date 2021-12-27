a5fba8183545d5b720314f38475146ff7400946f
HAoRAn-W <haoran2326@gmail.com>
2021-12-27 00:02:40 -0500

    add automatic deployment

--- RyanBlog/settings/common.py
+++ RyanBlog/settings/common.py
@@ -13,18 +13,18 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_mu7u$r5&1%dv!0p7u1(lk91)3dv-ru(9_!nusijqocbn(^*4t'
+# SECRET_KEY = '_mu7u$r5&1%dv!0p7u1(lk91)3dv-ru(9_!nusijqocbn(^*4t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '20.124.206.155', '.whr.one']
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '20.124.206.155', '.whr.one']
 
 # Application definition
 
---
+++ RyanBlog/settings/local.py
@@ -0,0 +1,5 @@
+from .common import *
+
+SECRET_KEY = '_mu7u$r5&1%dv!0p7u1(lk91)3dv-ru(9_!nusijqocbn(^*4t'
+DEBUG = True
+ALLOWED_HOSTS = ['*']
